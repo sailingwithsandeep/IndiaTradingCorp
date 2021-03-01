2f45c0d20ce1e1e35fd38bb1f977e2ae2be60a15
SANDEEP-PARMAR-RKU <sparmar586@rku.ac.in>
2021-03-01 20:45:22 +0530

    final touch responsive bugs needs to be solved

--- ITC/urls.py
+++ ITC/urls.py
@@ -23,10 +23,6 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls', namespace='products')),
-    re_path(r'^media/(?P<path>.*)$', serve,
-            {'document_root': settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve,
-            {'document_root': settings.STATIC_ROOT}),
 
 
 
