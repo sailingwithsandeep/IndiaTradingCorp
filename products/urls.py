c1acb6e881f34e314b7b9b2ea44971c4a06ba5ef
SandeepParmar-RKU <sparmar586@rku.ac.in>
2021-03-24 11:38:32 +0530

    created model of about and contact us

--- products/urls.py
+++ products/urls.py
@@ -8,6 +8,7 @@ app_name = 'products'
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
     path('products', views.ProductPage.as_view(), name='productpage'),
+    path('about', views.AboutPage.as_view(), name='about'),
     path('saveemail', views.insertEmail, name='saveemail'),
 
 ]
